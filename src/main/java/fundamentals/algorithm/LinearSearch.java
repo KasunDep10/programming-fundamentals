2d07fdde3e7063b61fc6e3d8871eb3834f49b04d
kasun-chathuranga <kasunchathuranga3732@gmail.com>
2023-07-25 08:25:30 +0530

    LinearSearch by using While-loop

--- src/main/java/fundamentals/algorithm/LinearSearch.java
+++ src/main/java/fundamentals/algorithm/LinearSearch.java
@@ -3,6 +3,7 @@ package fundamentals.algorithm;
 public class LinearSearch {
     public void methodCall(int[] nums, int search){
         forLoop(nums, search);
+        whileLoop(nums, search);
     }
 
     private void forLoop(int[] nums, int search){
@@ -11,6 +12,18 @@ public class LinearSearch {
                 System.out.printf("%s is found at index: %d \n", search, i);
                 return;
             }
+        }
+        System.out.println("The searched number does not exist");
+    }
+
+    private void whileLoop(int[] nums, int search){
+        int i = 0;
+        while (i < nums.length){
+            if(nums[i] == search){
+                System.out.printf("%s is found at index: %d \n", search, i);
+                return;
+            }
+            i++;
         }
         System.out.println("The searched number does not exist");
     }
