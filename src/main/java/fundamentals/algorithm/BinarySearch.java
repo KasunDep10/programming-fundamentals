d7020c6dd9ef46426c00790d3ef8c10a227e90e9
kasun-chathuranga <kasunchathuranga3732@gmail.com>
2023-07-26 08:30:13 +0530

    BinarySearch by using DoWhile-loop

--- src/main/java/fundamentals/algorithm/BinarySearch.java
+++ src/main/java/fundamentals/algorithm/BinarySearch.java
@@ -3,6 +3,7 @@ package fundamentals.algorithm;
 public class BinarySearch {
     public void methodCall(int[] nums, int search){
         whileLoop(nums, search);
+        doWhileLoop(nums, search);
     }
 
     public void whileLoop(int[] nums, int search){
@@ -24,6 +25,30 @@ public class BinarySearch {
                 start = mid + 1;
             }
         }
+        System.out.println("The searched number does not exist");
+    }
+
+    public void doWhileLoop(int[] nums, int search) {
+        int start = 0;
+        int end = nums.length;
+        int mid = 0;
+        if (search < nums[start] || search > nums[end - 1]) {
+            System.out.println("The searched number does not exist");
+            return;
+        }
+
+        do {
+            mid = (start + end) / 2;
+            if (search == nums[mid]) {
+                System.out.printf("%s is found at index: %d \n", search, mid);
+                return;
+            } else if (search < nums[mid]) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        } while (start < end);
+
         System.out.println("The searched number does not exist");
     }
 }
