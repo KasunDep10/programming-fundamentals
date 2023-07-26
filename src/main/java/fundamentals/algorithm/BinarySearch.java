38b3831dd5886bec425f897ce877d7952c45b5e2
kasun-chathuranga <kasunchathuranga3732@gmail.com>
2023-07-26 08:56:22 +0530

    BinarySearch by using For-loop

--- src/main/java/fundamentals/algorithm/BinarySearch.java
+++ src/main/java/fundamentals/algorithm/BinarySearch.java
@@ -2,11 +2,39 @@ package fundamentals.algorithm;
 
 public class BinarySearch {
     public void methodCall(int[] nums, int search){
+        forLoop(nums, search);
         whileLoop(nums, search);
         doWhileLoop(nums, search);
     }
 
-    public void whileLoop(int[] nums, int search){
+
+    private void forLoop(int[] nums, int search){
+        int start = 0;
+        int end = nums.length;
+        int mid = 0;
+        if (search < nums[start] || search > nums[end - 1]) {
+            System.out.println("The searched number does not exist");
+            return;
+        }
+
+        for( ; true ; ){
+            mid = (start + end)/2;
+            if(start >= end) {
+                System.out.println("The searched number does not exist");
+                return;
+            }
+            if(search == nums[mid]){
+                System.out.printf("%s is found at index: %d \n", search, mid);
+                return;
+            } else if(search < nums[mid]){
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+    }
+
+    private void whileLoop(int[] nums, int search){
         int start = 0;
         int end = nums.length;
         int mid = 0;
@@ -28,7 +56,7 @@ public class BinarySearch {
         System.out.println("The searched number does not exist");
     }
 
-    public void doWhileLoop(int[] nums, int search) {
+    private void doWhileLoop(int[] nums, int search) {
         int start = 0;
         int end = nums.length;
         int mid = 0;
