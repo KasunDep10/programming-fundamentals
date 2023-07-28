71932f53a52572a37455e1f1f6a344c244da7cfc
kasun-chathuranga <kasunchathuranga3732@gmail.com>
2023-07-28 08:45:25 +0530

    BubbleSort using While Loop

--- src/main/java/fundamentals/algorithm/BubbleSort.java
+++ src/main/java/fundamentals/algorithm/BubbleSort.java
@@ -6,6 +6,8 @@ public class BubbleSort {
     public void methodCall(int[] nums) {
         forLoopMin(nums);
         forLoopMax(nums);
+        whileMin(nums);
+        whileMax(nums);
     }
 
 
@@ -36,5 +38,43 @@ public class BubbleSort {
             }
         }
         System.out.println("forLoopMax " + Arrays.toString(nums));
+    }
+
+
+    private void whileMin(int[] nums){
+        int i = nums.length - 1;
+        while (i > 0){
+            int temp;
+            int j = nums.length - 1;
+            while (j > nums.length - 1 - i){
+                if(nums[j] < nums[j - 1]){
+                    temp = nums[j];
+                    nums[j] = nums[j - 1];
+                    nums[j - 1] = temp;
+                }
+                j--;
+            }
+            i--;
+        }
+        System.out.println("whileMin " + Arrays.toString(nums));
+    }
+
+    private void whileMax(int[] nums){
+        int i = 0;
+        while (i < nums.length - 1) {
+            int temp;
+            int j = 0;
+            while (j < nums.length - 1 - i) {
+                if (nums[j] > nums[j + 1]) {
+                    temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+                j++;
+            }
+            i++;
+        }
+
+        System.out.println("whileMax " + Arrays.toString(nums));
     }
 }
