b6fed8a12252461aaab7bce584375e53fe5a45d6
kasun-chathuranga <kasunchathuranga3732@gmail.com>
2023-07-29 11:12:36 +0530

    LinearInsertionSort by using Recursion

--- src/main/java/fundamentals/algorithm/LinearInsertionSort.java
+++ src/main/java/fundamentals/algorithm/LinearInsertionSort.java
@@ -7,6 +7,8 @@ public class LinearInsertionSort {
         forLoop(nums);
         whileLoop(nums);
         doWhileLoop(nums);
+//        recursion(nums, 1);
+        insertionSort(nums, 1);
     }
 
     private void forLoop(int[] nums) {
@@ -73,4 +75,67 @@ public class LinearInsertionSort {
         } while (i < nums.length);
         System.out.println("DoWhile Loop " + Arrays.toString(nums));
     }
+
+    private void recursion(int[] nums, int i){
+        if(i == nums.length) {
+            System.out.println("Recursion " + Arrays.toString(nums));
+            return;
+        }
+        if(nums[i] < nums[i-1]){
+            int j = 0;
+            int temp = nums[i];
+            recursion2(nums, temp, i, j);
+            return;
+        }
+        i++;
+        recursion(nums, i);
+
+    }
+
+    private void recursion2(int[] nums, int temp, int i, int j){
+        if(j >= i) {
+            return;
+        }
+        if(temp < nums[j]){
+            recursion3(nums, j, i);
+            nums[j] = temp;
+            return;
+        }
+        j++;
+        recursion2(nums, temp, i, j);
+    }
+
+    private void recursion3(int[] nums, int j, int k){
+        if(k <= j) {
+            return;
+        }
+        nums[k] = nums[k - 1];
+        k--;
+        recursion3(nums, j, k);
+    }
+
+
+    private void insertionSort(int[] nums, int i) {
+        if (i == nums.length) {
+            System.out.println("Sorted Array: " + Arrays.toString(nums));
+            return;
+        }
+
+        int temp = nums[i];
+        insertElement(nums, temp, i);
+        i++;
+        insertionSort(nums, i);
+    }
+
+    private void insertElement(int[] nums, int temp, int i) {
+        if (i <= 0 || temp >= nums[i - 1]) {
+            nums[i] = temp;
+            return;
+        }
+
+        nums[i] = nums[i - 1];
+        i--;
+        insertElement(nums, temp, i);
+    }
+
 }
