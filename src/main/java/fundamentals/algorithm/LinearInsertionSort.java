190ddfb8fcd101d679045c013c5ceb5acfc3e1ef
kasun-chathuranga <kasunchathuranga3732@gmail.com>
2023-07-29 09:55:21 +0530

    LinearInsertionSort by using While Loop

--- src/main/java/fundamentals/algorithm/LinearInsertionSort.java
+++ src/main/java/fundamentals/algorithm/LinearInsertionSort.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class LinearInsertionSort {
     public void methodCall(int[] nums) {
         forLoop(nums);
+        whileLoop(nums);
     }
 
     private void forLoop(int[] nums) {
@@ -23,6 +24,29 @@ public class LinearInsertionSort {
             }
         }
 
-        System.out.println(Arrays.toString(nums));
+        System.out.println("For Loop " + Arrays.toString(nums));
+    }
+
+    private void whileLoop(int[] nums){
+        int i = 1;
+        while (i < nums.length){
+            if(nums[i] < nums[i-1]){
+                int temp = nums[i];
+                int j = 0;
+                while (j < i){
+                    if(temp < nums[j]){
+                        int k = i;
+                        while(k > j){
+                            nums[k] = nums[k - 1];
+                            k--;
+                        }
+                        nums[j] = temp;
+                    }
+                    j++;
+                }
+            }
+            i++;
+        }
+        System.out.println("While Loop " + Arrays.toString(nums));
     }
 }
